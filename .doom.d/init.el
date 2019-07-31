887ae6ce73817597761d97281c94e14ee00d6c32
Damien Ayers <damien@omad.net>
2019-08-01 09:42:24 +1000

    doom

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -14,14 +14,14 @@
 
        :completion
        (company           ; the ultimate code completion backend
-        +childframe
+        ;; +childframe
         +tng)
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy               ; a search engine for love and life
         +fuzzy
-        +icons
-        +childframe)
+        +icons)
+       ;; +childframe)
 
        :ui
        deft              ; notational velocity for Emacs
