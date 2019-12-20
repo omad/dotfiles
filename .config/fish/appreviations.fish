4c9cdc0805b0bb365eb821384af35902987b954d
Damien Ayers <damien@omad.net>
2019-12-20 12:03:15 +1100

    More fishing

---
+++ .config/fish/appreviations.fish
@@ -0,0 +1,42 @@
+
+abbr --add g 'git'
+abbr --add y 'yadm'
+
+# Git shortcuts
+abbr --add ga 'git add'
+abbr --add gaa 'git add .'
+abbr --add gb 'git branch'
+abbr --add gba 'git branch -a'
+abbr --add gbd 'git branch -D'
+abbr --add gbl 'git branch | grep "*" | sed "s/* //"'
+abbr --add gc 'git commit'
+abbr --add gca 'git commit -a'
+abbr --add gcd 'cd (git rev-parse --show-toplevel)'
+abbr --add gco 'git checkout'
+abbr --add gcob 'git checkout -b'
+abbr --add gcod 'git checkout development'
+abbr --add gcom 'git checkout master'
+abbr --add gcp 'git cherry-pick'
+abbr --add gcs 'git commit -sS'
+abbr --add gd 'git diff'
+abbr --add gfp 'git fetch --prune'
+abbr --add gi 'git ignore'
+abbr --add gl "git log --pretty='format:%C(Yellow)%h%Creset %C(Blue)%ar%Creset %an - %s' --graph"
+abbr --add gm 'git merge --no-ff'
+abbr --add gpoh 'git push origin HEAD'
+abbr --add gpull 'git pull'
+abbr --add gpush 'git push'
+abbr --add grh 'git reset --hard'
+abbr --add gst 'git status'
+abbr --add gt 'git tag'
+abbr --add gts 'git tag -s'
+abbr --add lg 'lazygit'
+abbr --add ts 'tig status'
+
+# yadm shortcuts
+abbr --add ya 'yadm add'
+abbr --add yc 'yadm commit'
+abbr --add yd 'yadm diff'
+abbr --add yst 'yadm status'
+
+abbr --add mkdir 'mkdir -pv'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,39 @@ if test -f /opt/Modules/v4.3.0/init/fish
     source /opt/Modules/v4.3.0/init/fish
     source /opt/Modules/v4.3.0/init/fish_completion
 end
+
+set -gx MANPAGER 'less -X'
+
+# Colorize man
+set MANROFFOPT '-c'
+set LESS_TERMCAP_mb (tput bold; tput setaf 2)
+set LESS_TERMCAP_md (tput bold; tput setaf 6)
+set LESS_TERMCAP_me (tput sgr0)
+set LESS_TERMCAP_so (tput bold; tput setaf 3; tput setab 4)
+set LESS_TERMCAP_se (tput rmso; tput sgr0)
+set LESS_TERMCAP_us (tput smul; tput bold; tput setaf 7)
+set LESS_TERMCAP_ue (tput rmul; tput sgr0)
+set LESS_TERMCAP_mr (tput rev)
+set LESS_TERMCAP_mh (tput dim)
+
+# pyenv
+if test -d $HOME/.pyenv
+	set -x PYENV_ROOT $HOME/.pyenv
+	set -x PATH $PYENV_ROOT/bin $PATH
+	source (pyenv init -|psub) > /dev/null 2>&1
+end
+
+# Go
+if test -d $HOME/go
+	set GOPATH $HOME/go
+	set PATH $PATH $GOPATH/bin
+end
+
+# fzf
+if test -d $HOME/.fzf/shell
+	source $HOME/.fzf/shell/key-bindings.fish
+	set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+end
+
+# Source command abbreviations
+source $HOME/.config/fish/abbreviations.fish > /dev/null 2>&1
