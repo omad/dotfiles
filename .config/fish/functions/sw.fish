be2b9fa31eb0191d6cf75012034be669024a0ea1
Damien Ayers <damien@omad.net>
2020-06-10 18:05:13 +1000

    Improve app launcher fish function

--- .config/fish/functions/sw.fish
+++ .config/fish/functions/sw.fish
@@ -1,10 +1,18 @@
-# Defined in /tmp/fish.Xg8mzE/sw.fish @ line 2
+# Defined in /tmp/fish.jk5fS3/sw.fish @ line 2
 function sw --description 'Start Windows Environment'
-    xrandr --setmonitor default~1 1920/254x1080/286+0+0 default
-    xrandr --setmonitor default~2 1920/255x1080/286+1920+268 none
+    # Setup X11 virtual monitors, if not done already
+    if xrandr --listactivemonitors | not grep -q 'default~2'
+        xrandr --setmonitor default~1 1920/254x1080/286+0+0 default
+        xrandr --setmonitor default~2 1920/255x1080/286+1920+268 none
+    end
 
     xrdb -load $HOME/.Xresources
-    fenv "eval `dbus-launch`"
+
+    if set -q DBUS_SESSION_BUS_ADDRESS
+        fenv "eval `dbus-launch --sh-syntax`"
+        echo "D-Bus per-session daemon address is: $DBUS_SESSION_BUS_ADDRESS"
+    end
+
     fenv "eval `gnome-keyring-daemon -r -d -c secrets,ssh,pkcs11`"
     # pycharm > /dev/null 2>&1 &
     urxvt &
