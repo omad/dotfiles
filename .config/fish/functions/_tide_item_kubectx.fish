eb7ec8698097cc675b5854cc4f6b3368413126cb
Damien Ayers <damien@omad.net>
2021-03-04 16:11:26 +1100

    git and tmux and fish shell

--- .config/fish/functions/_tide_item_kubectx.fish
+++ .config/fish/functions/_tide_item_kubectx.fish
@@ -1,5 +1,8 @@
 function _tide_item_kubectx
-    set_color bryellow
-    set -l current_kubectx (kubectx -c 2> /dev/null)
-    test -n "$current_kubectx"; and echo -n 'ﴱ ' $current_kubectx
+    if type -q kubectx
+        set_color bryellow
+        set -l current_kubectx (kubectx -c 2> /dev/null)
+        test -n "$current_kubectx"; and echo -n 'ﴱ ' $current_kubectx
+    end
+
 end
