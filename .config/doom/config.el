8d4442d6ee8b38f2085c42b0dedf3f5cd0b387ec
Damien Ayers <damien@omad.net>
2021-05-17 21:15:35 +1000

    dooom agenda

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -39,8 +39,36 @@
 
   (org-super-agenda-mode))
 
+(defun dra/org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
 (setq org-agenda-span 'week
-      org-agenda-todo-list-sublevels nil) ; only list top level TODOs
+      org-agenda-todo-list-sublevels nil
+                                        ; Thanks https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+      org-agenda-custom-commands
+      '(("d" "Damien's Daily"
+         ((tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "High-priority unfinished tasks:")))
+          (agenda "" ((org-agenda-start-day nil)
+                      (org-agenda-span 'day)))
+          (alltodo ""
+                   ((org-agenda-skip-function
+                     '(or (dra/org-skip-subtree-if-priority ?A)
+                          (org-agenda-skip-if nil '(scheduled deadline))))
+                    (org-agenda-overriding-header "ALL normal priority tasks:")))))))
+                                        ;          (tags-todo "+PRIORITY=\"B\""))
+;         (org-agenda-sorting-strategy '(priority-down))
+;         (org-agenda-start-day nil)
+;         (org-agenda-span 'day)))) ; only list top level TODOs
 
 (use-package projectile-git-autofetch
   :after projectile magit
