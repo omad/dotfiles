2b16d13b52d7ee9b01ca6f2683fa7db191b020c7
Damien Ayers <damien@omad.net>
2023-04-26 15:33:17 +1000

    CLI Preview of notebooks

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,11 +1,14 @@
 function cat --description 'Magic cat + mdcat + bat'
     set -l md_exts md markdown txt
     set -l img_exts png gif jpg jpeg
+    set -l nb_exts ipynb
 
     if not isatty stdout
         command cat $argv
     else if contains (get_ext $argv) $md_exts
         mdcat --paginate $argv
+    else if contains (get_ext $argv) $nb_exts; and type -q nbpreview
+        nbpreview $argv
     else if contains (get_ext $argv) $img_exts; and test $TERM = xterm-kitty
         command kitty +kitten icat $argv
     else
