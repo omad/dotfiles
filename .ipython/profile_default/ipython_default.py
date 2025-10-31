de9a020f6e773f8a3efab0a77558273fa872b2e5
Damien Ayers <damien@omad.net>
2025-10-31 11:12:20 +1100

    updates

---
+++ .ipython/profile_default/ipython_default.py
@@ -0,0 +1,5 @@
+c.TerminalInteractiveShell.auto_complete_while_typing = True
+c.TerminalInteractiveShell.completion_limit = 60
+c.TerminalInteractiveShell.true_color = True
+c.TerminalInteractiveShell.space_for_menu = 14
+c.TerminalInteractiveShell.true_color = True
