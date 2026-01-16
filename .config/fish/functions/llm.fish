a6f513ec3cd431dea253291247d7c4fa2c53c366
Damien Ayers <damien@omad.net>
2026-01-16 17:44:21 +1100

    helpful shell tools

---
+++ .config/fish/functions/llm.fish
@@ -0,0 +1,12 @@
+function llm
+  # Inspirationhttps://notes.billmill.org/dev_blog/2026/01/mdstream_-_a_vibecoding_experiment.html
+
+  if type -q mdriver
+    command llm "$argv" | mdriver
+  else if type -q bat
+    command llm "$argv" | bat --paging=never --style=plain --language=markdown
+  else
+    command llm "$argv"
+  end
+
+end
