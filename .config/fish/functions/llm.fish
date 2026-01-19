5b855c8a3c82046c29a2b8e2d3b76987be900f60
Damien Ayers <damien@omad.net>
2026-01-20 10:27:29 +1100

    Fix llm wrapper

--- .config/fish/functions/llm.fish
+++ .config/fish/functions/llm.fish
@@ -2,11 +2,11 @@ function llm
   # Inspirationhttps://notes.billmill.org/dev_blog/2026/01/mdstream_-_a_vibecoding_experiment.html
 
   if type -q mdriver
-    command llm "$argv" | mdriver
+    command llm $argv | mdriver
   else if type -q bat
-    command llm "$argv" | bat --paging=never --style=plain --language=markdown
+    command llm $argv | bat --paging=never --style=plain --language=markdown
   else
-    command llm "$argv"
+    command llm $argv
   end
 
 end
